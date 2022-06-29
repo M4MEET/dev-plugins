8106be0f27cb02e06c353ba7ba5d6259cdbf48c2
xtrawatt <joshi@swissbatt24.ch>
2022-06-29 14:42:36 +0200

    theme json file changes

---
+++ ThemeBattronGmbh/src/Resources/app/storefront/dist/storefront/js/theme-battron-gmbh.js
@@ -0,0 +1,14 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["theme-battron-gmbh"],{
+
+/***/ "iZwh":
+/*!*************************************************************************************!*\
+  !*** /app/custom/plugins/ThemeBattronGmbh/src/Resources/app/storefront/src/main.js ***!
+  \*************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaVp3aC5qcyIsInNvdXJjZXMiOltdLCJtYXBwaW5ncyI6IiIsInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///iZwh\n");
+
+/***/ })
+
+},[["iZwh","runtime"]]]);
---
+++ ThemeBattronGmbh/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'shopware-platform-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'bd5b3661fa64bcdb8d6c459d9a8814d72e03b51b',
+        'name' => 'battron/theme-battron',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'battron/theme-battron' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'shopware-platform-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'bd5b3661fa64bcdb8d6c459d9a8814d72e03b51b',
+            'dev_requirement' => false,
+        ),
+    ),
+);
